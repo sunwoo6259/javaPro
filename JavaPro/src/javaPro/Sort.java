fde07c3131c40acc0b9756ec8923fade2fc1f9d1
sunwoo <rlatjsdn6259@naver.com>
2024-07-15 14:52:32 +0900

    정렬 추가 샘플 수정

--- JavaPro/src/javaPro/Sort.java
+++ JavaPro/src/javaPro/Sort.java
@@ -1,12 +1,9 @@
 package javaPro;
 
-public class Sample {
+public class Sort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("김선우");
-		System.out.println("송세호");
 
 	}
 
